92814a2be9c6cdb5341019f47a4f6dadd311bbc4
Artsiom Kurlovich <qa.a.kurlovich@gmail.com>
2023-01-27 15:16:44 +0300

    Update index.ts

--- src/index.ts
+++ src/index.ts
@@ -64,193 +64,195 @@ const queueArray: ITask[] = [
   { targetId: 4, action: 'init' }, { targetId: 0, action: 'init' }, { targetId: 1, action: 'init' },
   { targetId: 6, action: 'init' }, { targetId: 1, action: 'prepare' }, { targetId: 8, action: 'init' },]
 
-  class Queue {
-    paused: boolean;
-    concurrency: number;
-    count: number;
-    waiting: ITaskWaiting[];
-    onProcess: (task: ITask, finish: (err: Error, res: ITask) => void) => void;
-    onDone: (err: Error, res: ITask) => void;
-    onSuccess: (res: ITask) => void = null;
-    onFailure: (err: Error, res: ITask) => void;
-    onDrain: () => void = null;
-    waitTimeout = Infinity;
-    processTimeout = Infinity;
-    priorityMode = false;
-    destination: { (): void; add?: any; } = null;
+class Queue {
+  paused: boolean;
+  concurrency: number;
+  count: number;
+  waiting: ITaskWaiting[];
+  onProcess: (task: ITask, finish: (err: Error, res: ITask) => void) => void;
+  onDone: (err: Error, res: ITask) => void;
+  onSuccess: (res: ITask) => void = null;
+  onFailure: (err: Error, res: ITask) => void;
+  onDrain: () => void = null;
+  waitTimeout = Infinity;
+  processTimeout = Infinity;
+  priorityMode = false;
+  destination: { (): void; add?: any; } = null;
 
-    constructor(concurrency: number) {
-      this.paused = false;
-      this.concurrency = concurrency;
-      this.count = 0;
-      this.waiting = [];
-      this.onProcess = null;
-      this.onDone = null;
-      this.onSuccess = null;
-      this.onFailure = null;
-      this.onDrain = null;
-      this.waitTimeout = Infinity;
-      this.processTimeout = Infinity;
-      this.priorityMode = false;
-      this.destination = null;
-    }
-  
-    static channels(concurrency: number) {
-      return new Queue(concurrency);
-    }
-  
-    wait(msec: number) {
-      this.waitTimeout = msec;
-      return this;
-    }
-  
-    timeout(msec: number) {
-      this.processTimeout = msec;
-      return this;
-    }
-  
-    add(task: ITask, priority = 0) {
-      if (!this.paused) {
-        const hasChannel = this.count < this.concurrency;
-        if (hasChannel) {
-          this.next(task);
-          return;
-        }
-      }
-      this.waiting.push({ task, start: Date.now(), priority });
-      if (this.priorityMode) {
-        this.waiting.sort((a, b) => b.priority - a.priority);
-      }
-    }
-  
-    next(task: ITask) {
-      this.count++;
-      let timer: ReturnType<typeof setTimeout> = null;
-      let finished = false;
-      const { processTimeout, onProcess } = this;
-      const finish = (err: Error, res: ITask) => {
-        if (finished) return;
-        finished = true;
-        if (timer) clearTimeout(timer);
-        this.count--;
-        this.finish(err, res);
-        if (!this.paused && this.waiting.length > 0) this.takeNext();
-      };
-      if (processTimeout !== Infinity) {
-        const err = new Error('Process timed out');
-        timer = setTimeout(finish, processTimeout, err, task);
-      }
-      onProcess(task, finish);
-    }
-  
-    takeNext() {
-      const { waiting, waitTimeout } = this;
-      const { task, start } = waiting.shift();
-      if (waitTimeout !== Infinity) {
-        const delay = Date.now() - start;
-        if (delay > waitTimeout) {
-          const err = new Error('Waiting timed out');
-          this.finish(err, task);
-          if (waiting.length > 0) {
-            setTimeout(() => {
-              if (!this.paused && waiting.length > 0) this.takeNext();
-            }, 0);
-          }
-          return;
-        }
-      }
-      const hasChannel = this.count < this.concurrency;
-      if (hasChannel) this.next(task);
-      return;
-    }
-  
-    finish(err: Error, res: ITask) {
-      const { onFailure, onSuccess, onDone, onDrain } = this;
-      if (err) {
-        if (onFailure) onFailure(err, res);
-      } else {
-        if (onSuccess) onSuccess(res);
-        if (this.destination) this.destination.add(res);
-      }
-      if (onDone) onDone(err, res);
-      if (this.count === 0 && onDrain) onDrain();
-    }
-  
-    process(listener: (task: ITask, next: (err: Error, res: ITask) => void) => void) {
-      this.onProcess = listener;
-      return this;
-    }
-  
-    done(listener: (err: Error, res: ITask) => void) {
-      this.onDone = listener;
-      return this;
-    }
-  
-    success(listener: (res: ITask) => void) {
-      this.onSuccess = listener;
-      return this;
-    }
-  
-    failure(listener: (err: Error, res: ITask) => void) {
-      this.onFailure = listener;
-      return this;
-    }
-  
-    drain(listener: () => void) {
-      this.onDrain = listener;
-      return this;
-    }
-  
-    pause() {
-      this.paused = true;
-      return this;
-    }
-  
-    resume() {
-      if (this.waiting.length > 0) {
-        const channels = this.concurrency - this.count;
-        for (let i = 0; i < channels; i++) {
-          this.takeNext();
-        }
-      }
-      this.paused = false;
-      return this;
-    }
-  
-    priority(flag = true) {
-      this.priorityMode = flag;
-      return this;
-    }
-  
-    pipe(destination: () => void) {
-      this.destination = destination;
-      return this;
-    }
+  constructor(concurrency: number) {
+    this.paused = false;
+    this.concurrency = concurrency;
+    this.count = 0;
+    this.waiting = [];
+    this.onProcess = null;
+    this.onDone = null;
+    this.onSuccess = null;
+    this.onFailure = null;
+    this.onDrain = null;
+    this.waitTimeout = Infinity;
+    this.processTimeout = Infinity;
+    this.priorityMode = false;
+    this.destination = null;
   }
+
+  static channels(concurrency: number) {
+    return new Queue(concurrency);
+  }
+
+  wait(msec: number) {
+    this.waitTimeout = msec;
+    return this;
+  }
+
+  timeout(msec: number) {
+    this.processTimeout = msec;
+    return this;
+  }
+
+  add(task: ITask, priority = 0) {
+    if (!this.paused) {
+      const hasChannel = this.count < this.concurrency;
+      if (hasChannel) {
+        this.next(task);
+        return;
+      }
+    }
+    // this.waiting.push({ task, start: Date.now(), priority });
+    this.waiting.push({ task, start: Date.now(), priority });
+    // if (this.priorityMode) {
+    //   this.waiting.sort((a, b) => b.priority - a.priority);
+    // }
+  }
+
+  next(task: ITask) {
+    this.count++;
+    let timer: ReturnType<typeof setTimeout> = null;
+    let finished = false;
+    const { processTimeout, onProcess } = this;
+    const finish = (err: Error, res: ITask) => {
+      if (finished) return;
+      finished = true;
+      if (timer) clearTimeout(timer);
+      this.count--;
+      this.finish(err, res);
+      if (!this.paused && this.waiting.length > 0) this.takeNext();
+    };
+    if (processTimeout !== Infinity) {
+      const err = new Error('Process timed out');
+      timer = setTimeout(finish, processTimeout, err, task);
+    }
+    onProcess(task, finish);
+  }
+
+  takeNext() {
+    const { waiting, waitTimeout } = this;
+    const { task, start } = waiting.shift();
+    if (waitTimeout !== Infinity) {
+      const delay = Date.now() - start;
+      if (delay > waitTimeout) {
+        const err = new Error('Waiting timed out');
+        this.finish(err, task);
+        if (waiting.length > 0) {
+          setTimeout(() => {
+            if (!this.paused && waiting.length > 0) this.takeNext();
+          }, 0);
+        }
+        return;
+      }
+    }
+    const hasChannel = this.count < this.concurrency;
+    if (hasChannel) this.next(task);
+    return;
+  }
+
+  finish(err: Error, res: ITask) {
+    const { onFailure, onSuccess, onDone, onDrain } = this;
+    if (err) {
+      if (onFailure) onFailure(err, res);
+    } else {
+      if (onSuccess) onSuccess(res);
+      if (this.destination) this.destination.add(res);
+    }
+    if (onDone) onDone(err, res);
+    if (this.count === 0 && onDrain) onDrain();
+  }
+
+  process(listener: (task: ITask, next: (err: Error, res: ITask) => void) => void) {
+    this.onProcess = listener;
+    return this;
+  }
+
+  done(listener: (err: Error, res: ITask) => void) {
+    this.onDone = listener;
+    return this;
+  }
+
+  success(listener: (res: ITask) => void) {
+    this.onSuccess = listener;
+    return this;
+  }
+
+  failure(listener: (err: Error, res: ITask) => void) {
+    this.onFailure = listener;
+    return this;
+  }
+
+  drain(listener: () => void) {
+    this.onDrain = listener;
+    return this;
+  }
+
+  pause() {
+    this.paused = true;
+    return this;
+  }
+
+  resume() {
+    if (this.waiting.length > 0) {
+      const channels = this.concurrency - this.count;
+      for (let i = 0; i < channels; i++) {
+        this.takeNext();
+      }
+    }
+    this.paused = false;
+    return this;
+  }
+
+  priority(flag = true) {
+    this.priorityMode = flag;
+    return this;
+  }
+
+  pipe(destination: () => void) {
+    this.destination = destination;
+    return this;
+  }
+}
   
   // Usage
 
-  const job = (task: ITask, next: (err: Error, res: ITask) => void) => {
-    console.log(`Process targetId: ${task.targetId}`);
-    setTimeout(next, 2000, null, task);
-  };
-  
-  const queue = Queue.channels(3)
-    .process(job)
-    .done((err: Error, res: ITask) => {
-      const { count } = queue;
-      const waiting = queue.waiting.length;
-      console.log(`Done targetId: ${res.targetId}, count:${count}, waiting: ${waiting}`);
-    })
-    .success((res: ITask) => console.log(`Success targetId: ${res.targetId}`))
-    // .failure((res: ITask) => console.log(`Failure targetId: ${err}`))
-    .failure((err: Error, res: ITask) => console.log(`Failure: ${err}`))
-    .drain(() => console.log('Queue drain'));
-  
-  for (let i = 0; i < queueArray.length; i++) {
-    // queue.add({ name: `Task${i}`, interval: i * 1000 });
-    queue.add(queueArray[i]);
-  }
+const job = (task: ITask, next: (err: Error, res: ITask) => void) => {
+  console.log(`Process targetId: ${task.targetId}`);
+  // next(null, task)
+  setTimeout(next, 0, null, task);
+};
+
+const queueArr = Queue.channels(3)
+  .process(job)
+  .done((err: Error, res: ITask) => {
+    const { count } = queueArr;
+    const waiting = queueArr.waiting.length;
+    console.log(`Done targetId: ${res.targetId}, count:${count}, waiting: ${waiting}`);
+  })
+  .success((res: ITask) => console.log(`Success targetId: ${res.targetId}`))
+  // .failure((res: ITask) => console.log(`Failure targetId: ${err}`))
+  .failure((err: Error, res: ITask) => console.log(`Failure: ${err}`))
+  .drain(() => console.log('Queue drain'));
+
+for (let i = 0; i < queueArray.length; i++) {
+  // queue.add({ name: `Task${i}`, interval: i * 1000 });
+  queueArr.add(queueArray[i]);
+}
   
   // const destination = Queue.channels(2)
   //   .wait(5000)
